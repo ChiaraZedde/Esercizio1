058ae7c0e2c2324a0c61412d7615c1c27272c71f
Chiara Zedde <chiarazedde2000@gmail.com>
2024-04-10 15:50:12 +0200

    questa è una modifica fatta per mostrare come si utilizzano le branch e il merge

--- AggiungiNuovoContatto.cs
+++ AggiungiNuovoContatto.cs
@@ -2,3 +2,6 @@ Console.Writeline("Inserisci un nuovo contatto");
 int nomeCompleto =Console.ReadLine();
 
 Console.Writeline($"Il nome che è stato inserito è {nomeCompleto}, è corretto ?")
+
+
+Console.Writeline ("Ciaooooooooo modifica 2")
